c90cd0cadbb08ea20d0945620993117f4f211670
rupertlion <rupertlion@gmail.com>
2018-10-09 11:35:54 +0200

    Connecting user to performance data - all unit tests passing

--- app/controllers/api/v1/performance_data_controller.rb
+++ app/controllers/api/v1/performance_data_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PerformanceDataController < ApplicationController
     def create
-        @data = PerformanceData.new(performance_data_params)
+        @data = PerformanceData.new(performance_data_params.merge(user: current_api_v1_user))
         if @data.save
             render json: { message: 'all good' }
         else
