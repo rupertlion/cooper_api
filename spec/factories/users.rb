e0aaf33dcf7076f68425d4245f4d9dbba6e4652b
rupertlion <rupertlion@gmail.com>
2018-10-09 10:10:20 +0200

    Creating user_spec.rb and tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    email { "MyString" }
-    password { "MyString" }
-    password_confirmation { "MyString" }
+    email { "user@random.com" }
+    password { "password" }
+    password_confirmation { "password" }
   end
 end
